07809ea1261606dffddc044b39004daae5e113c4
Taddes Korris <taddes.korris@passportinc.com>
2019-07-10 21:31:36 -0400

    caesar hack function

---
+++ caesar-cipher/caeser_hack.py
@@ -0,0 +1,34 @@
+"""
+Implementation of how to hack the Caeser Cipher with brute force
+"""
+import timeit
+
+message = 'guv6Jv6Jz!J6rp5r7Jzr66ntrM'
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+
+
+def crack_caesar(message, symbols):
+# Iterate through all possible keys
+    for key in range(len(SYMBOLS)):
+        # Important is to set translated to the blank string so that the previous interation value for translation is cleared
+
+        translated = ''
+        for symbol in message:
+            if symbol in SYMBOLS:
+                  symbol_index = SYMBOLS.find(symbol)
+                  translated_index = symbol_index - key
+
+                  # Handle circling back past final symbols
+                  if translated_index < 0:
+                      translated_index = translated_index + len(SYMBOLS)
+
+                  # Append the decrypted symbol
+                  translated = translated + SYMBOLS[translated_index]
+
+            else:
+                # Append symbol without encrypting/decrypting
+                translated = translated + symbol
+
+        print('Key #%s: %s' % (key, translated))
+
+crack_caesar(message, SYMBOLS)
