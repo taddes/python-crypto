43c595b29a0b18ec596d0203dab0d30ff7675c88
Taddes Korris <taddes.korris@passportinc.com>
2019-08-12 21:56:21 -0400

    transp cipher

--- transposition_cipher/transp_cipher.py
+++ transposition_cipher/transp_cipher.py
@@ -1,6 +1,20 @@
+import pyperclip
+
 def main():
     my_message = 'Common sense is not so common.'
     my_key = 8
+
+    ciphertext = encrypt_message(my_key, my_message)
+    # Print encrypted ciphertext to screen with a | ('pipe') delimeter to indicate end of message
+    print(ciphertext + '|')
+
+    # Copy the ciphertext to clipboard
+    pyperclip.copy(ciphertext)
+
+
+def encrypt_message(key, message):
+    
+
 
 if __name__ == '__main__':
     main()
