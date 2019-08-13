293b2c05907a492038d237cc3747cf7fc6ad1ec3
Taddes Korris <taddes.korris@passportinc.com>
2019-08-12 22:05:17 -0400

    testing

--- transposition_cipher/transp_cipher.py
+++ transposition_cipher/transp_cipher.py
@@ -13,7 +13,23 @@ def main():
 
 
 def encrypt_message(key, message):
-    
+    # Creates empty ciphertext list corresponding to key
+    # with a length of n-1, being the key of the cipher -1 to index from 0.
+    ciphertext = [''] * key
+
+    # Loop through each column in the cipher text 
+    for column in range(key):
+        current_index = column
+        print(current_index)
+
+        # Keep looping until current_index goes past message length
+        while current_index < len(message):
+            # Place char at current_index in message at the end of current column in the ciphertext list
+            ciphertext[column] += message[current_index]
+            print(ciphertext)
+            # Move current_index over by adding key value
+            current_index += key
+
 
 
 if __name__ == '__main__':
