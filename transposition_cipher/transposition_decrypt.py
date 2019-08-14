0805c4a733dda941052fef6f1ca1962b9a46e5bf
Taddes Korris <taddes.korris@passportinc.com>
2019-08-14 11:50:55 -0400

    complete cipher

---
+++ transposition_cipher/transposition_decrypt.py
@@ -0,0 +1,42 @@
+import math, pyperclip
+
+def main():
+    my_message = 'Cenoonommstmme oo snnio. s s c'
+    my_key = 8
+
+    plaintext = decrypt_message(my_key, my_message)
+
+    # Print with '|' at end of message in case there are spaces at end
+    print(plaintext + '|')
+    pyperclip.copy(plaintext)
+
+def decrypt_message(key, message):
+    """Simulate the columns and rows of the frid that plaintext is written on, using alist of strings"""
+    # Columns in transposition grid
+    num_of_columns = int(math.ceil(len(message) / float(key)))
+    # Rows in transposition grid
+    num_of_rows = key
+    # Shaded or empty spaces in the last column of the grid
+    num_of_deadspace = (num_of_columns * num_of_rows) - len(message)
+    # Each string in plaintext represents a column in transposition grid
+    plaintext = [''] * num_of_columns
+    # Column and row variables point to where in the grid the net char in message will go
+    column = 0
+    row = 0
+
+    for symbol in message:
+        plaintext[column] += symbol
+        column += 1 # Point to next col
+
+        # If there are no more columns or we're at an eunused space, go back to first col and the next row
+        if (column == num_of_columns) or (column == num_of_columns - 1 and row >= num_of_rows - num_of_deadspace):
+            column = 0
+            row += 1
+
+    return ''.join(plaintext)
+
+if __name__ == '__main__':
+    main()
+
+
+  
