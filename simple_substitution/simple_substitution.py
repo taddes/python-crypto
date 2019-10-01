2a223052b81d8b95614bef7af1ff3d3c21c9ad13
Taddes Korris <taddes.korris@passportinc.com>
2019-10-01 11:53:14 -0400

    simple substitution main module

--- simple_substitution/simple_substitution.py
+++ simple_substitution/simple_substitution.py
@@ -21,5 +21,43 @@ def main():
     my_key = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
     my_mode = 'encrypt'
     # my_mode = 'decrypt'
+
+    if key_is_valid(my_key):
+        sys.exit('There is an error in the key or symbol set.')
+    if my_mode == 'encrypt':
+        translated = encrypt_message(my_key, my_message)
+    elif my_mode == 'decrypt':
+        translated = decrypt_message(my_key, my_message)
+    print('using key %s' % (my_key))
+    print('The %sed message is: ' % (my_mode))
+    print(translated)
+    pyperclip.copy(translated)
+    print()
+    print('Message copied to clipboard')
+
+
+def key_is_valid(key):
+    '''Determine validity of key by comparing to possible characters'''
+    key_list = list(key)
+    letters_list = list(LETTERS)
+    key_list.sort()
+    letters_list.sort()
+
+    return key_list == letters_list
+
+
+def encrypt_message(key, message):
+    return translate_message(key, message, 'encrypt')
+
+
+def decrypt_message(key, message):
+    return translate_message(key, message, 'decrypt')
+
+
+def translate_message(key, message, mode):
+    translated = ''
+    
+
+
 if __name__ == '__main__':
     main()
