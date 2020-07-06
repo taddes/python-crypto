6080a70f520b96814060360decd83c6f9dcf7f8a
Taddes Korris <taddes.korris@gmail.com>
2020-07-06 18:10:38 -0400

    changes

--- caesar-cipher/simple_implementation/caesar_cipher.py
+++ caesar-cipher/simple_implementation/caesar_cipher.py
@@ -10,7 +10,8 @@ def caesar_encrypt(plain_text, key=KEY):
     for char in plain_text:
         index = ALPHABET.find(char)
         index = (index + KEY) % len(ALPHABET)
-        cipher_text = cipher_text + ALPHABET[index]
+        # cipher_text = cipher_text + ALPHABET[index]
+        cipher_text += ALPHABET[index]
     
     return cipher_text
 
@@ -21,12 +22,15 @@ def caesar_decrypt(cipher_text, key):
     for char in cipher_text:
         index = ALPHABET.find(char)
         index = (index-KEY) % len(ALPHABET)
-        plain_text = plain_text + ALPHABET[index]
+        # plain_text = plain_text + ALPHABET[index]
+        plain_text += ALPHABET[index]
 
     return plain_text
+
 
 if __name__ == '__main__':
     encrypted = caesar_encrypt('There is no spoon', 12)
     print(f'Encrypted: {encrypted}')
     decrypted = caesar_decrypt(encrypted, 12)
     print(f'Decrypted: {decrypted}')
+    
