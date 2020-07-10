f7a865a0e8b282f4fa5a2162352ff40e20b08319
Taddes Korris <taddes.korris@gmail.com>
2020-07-10 17:12:53 -0400

    vig decryption

--- vigenere/vigenere_cipher.py
+++ vigenere/vigenere_cipher.py
@@ -19,12 +19,28 @@ def vigenere_encrypt(plain_text, key):
         # If we've considered the last letter of the key, we start again
         if key_index == len(key):
             key_index = 0
-            
+
     return cipher_text
 
 def vigenere_decrypt(cipher_text, key):
-    pass
+    cipher_text = cipher_text.upper()
+    key = key.upper()
+
+    plain_text = ''
+    key_index = 0
+
+    for char in cipher_text:
+        index = (ALPHABET.find(char) - (ALPHABET.find(key[key_index]))) % len(ALPHABET)
+        plain_text += ALPHABET[index]
+
+        key_index += 1
+
+        if key_index == len(key):
+            key_index = 0
+
+    return plain_text
 
 if __name__ == "__main__":
     encrypted = vigenere_encrypt('The world is so small yet so large', 'SPACEMAN')
     print(encrypted)
+    print(vigenere_decrypt(encrypted, 'SPACEMAN'))
