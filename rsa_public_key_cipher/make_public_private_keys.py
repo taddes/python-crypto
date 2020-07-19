606c8d3c2c923e256d9d450d4ed62a82c4fa77e9
Taddes Korris <taddes.korris@gmail.com>
2020-07-19 19:33:59 -0400

    RSA setup

--- rsa_public_key_cipher/make_public_private_keys.py
+++ rsa_public_key_cipher/make_public_private_keys.py
@@ -13,7 +13,36 @@ def main():
     """
     print('Generating keys')
     
+def generate_key(keysize):
+    """Creates public/private keys"""
+    p = 0
+    q = 0
 
+    # 1. Create two prime numbers, p and q, calculate n = p * q
+    print('Generating p prime')
+    while p == q:
+        p = prime_num.generate_large_prime_number(keysize)
+        q = prime_num.generate_large_prime_number(keysize)
+
+    n = p * q
+
+    # 2. Create e, that is relatively prime to (p - 1)  * (q -1)
+    print('Generating e that is relatively prime to (p-1) * (q-1')
+
+    while True:
+        # Keep trying random numbers for e until one is valid
+        e = random.randrange(2 **(keysize - 1), 2 ** (keysize))
+        if crypto_math.gcd(e, (p - 1) * (q - 1)) == 1:
+            break
+
+    # 3. Calculate d, the mod inverse of e
+    print('Calculating d, modular inverse of e.')
+    d = crypto_math.find_mod_inverse(e, (p - 1) * (q - 1))
+
+    public_key = (n, e)
+    private_key = (n, d)
+
+    return (public_key, private_key)
 def make_key_files(name, keysize):
     """
     Creates two files "x_pubkey.txt" and "x_privkey.txt" (where x
