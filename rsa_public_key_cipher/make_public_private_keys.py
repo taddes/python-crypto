886cd40e9247c8ec969e5624ef8f3f705c3dc8ac
Taddes Korris <taddes.korris@gmail.com>
2020-07-19 19:24:03 -0400

    RSA boilerplate

---
+++ rsa_public_key_cipher/make_public_private_keys.py
@@ -0,0 +1,18 @@
+"""
+    Public Key Generator using textbook RSA
+"""
+import random
+import sys
+import os
+import prime_num
+import crypto_math
+
+def main():
+    """
+        Create a private/public key pai with 1024-bit keys
+    """
+    print('Generating keys')
+    
+
+if __name__ == '__main__':
+    main()
